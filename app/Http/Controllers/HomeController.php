4263830457eef8fd801a30bffbaca60401ee9cbf
amir torkaman <amirtorkaman5204@gmail.com>
2020-11-21 22:09:10 +0330

    Add cache for index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use App\Http\Resources\Home\IndexAllAppsResource;
 use App\Http\Resources\Home\IndexResource;
 use App\Models\App;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Filesystem\Cache;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller {
     public function index( IndexRequest $request ) {
@@ -26,13 +26,30 @@ class HomeController extends Controller {
         $sort  = in_array( $request->orderby, $orders ) ? $request->orderby : 'installs';
         $order = $request->order == 'ASC' ? 'ASC' : 'DESC';
 
+
         /** @var LengthAwarePaginator $all_apps */
         $all_apps = App::groupBy( 'name' )->orderBy( $sort, $order )->paginate( $per_page );
         $all_apps = IndexResource::collection( $all_apps );
 
-        $best_apps = App::groupBy( 'name' )->orderByRaw( "(reviews/rating) DESC" )->get()->take( 10 );
 
-        $most_download = App::groupBy( 'name' )->orderByRaw( "length(installs) DESC" )->get()->take( 10 );
+        if ( ! Cache::has( 'index_best_apps' ) ) {
+            $best_apps = App::groupBy( 'name' )->orderByRaw( "(reviews/rating) DESC" )->get()->take( 10 );
+            $best_apps = IndexResource::collection( $best_apps );
+
+            Cache::put( 'index_best_apps', $best_apps, 60 * 60 ); //1 Hour cache
+        } else {
+            $best_apps = Cache::get( 'index_best_apps' );
+        }
+
+        if ( ! Cache::has( 'index_most_download' ) ) {
+            $most_download = App::groupBy( 'name' )->orderByRaw( "length(installs) DESC" )->get()->take( 10 );
+            $most_download = IndexResource::collection( $most_download );
+
+            Cache::put( 'index_most_download', $most_download, 60 * 60 ); //1 Hour cache
+        } else {
+            $most_download = Cache::get( 'index_most_download' );
+        }
+
 
         return [
             'all_apps'      => $all_apps,
